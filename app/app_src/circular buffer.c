9538bc5e0f76b0bfa6cc4de21be65d03a8826e1e
NidhiSura <76878978+NidhiSura@users.noreply.github.com>
2021-01-13 09:40:37 +0530

    Update circular buffer.c

--- app/app_src/circular buffer.c
+++ app/app_src/circular buffer.c
@@ -1,7 +1,7 @@
 /*
  * MIT License (MIT)
  *
- * Copyright (c) 2019 Kristian Kinderlöv
+ * Copyright (c) 2019 Kristian KinderlÃ¶v
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -35,7 +35,7 @@
 
 /* **** Includes **** */
 
-#include "circular buffer.h"
+#include "circularbuffer.h"
 #include <string.h>
 #include <assert.h>
 
@@ -60,7 +60,8 @@ void CircularBufferInit(CircularBufferContext *ctx, void *buf, size_t buf_size,
     ctx->write_pos = 0u;
     ctx->read_pos = 0u;
     ctx->element_size = element_size;
-    ctx->max_size = size - 1;
+    int sizeint = (int)size;
+    ctx->max_size = sizeint - 1;
 }
 
 void CircularBufferClear(CircularBufferContext *ctx) {
@@ -70,53 +71,63 @@ void CircularBufferClear(CircularBufferContext *ctx) {
 
 int8_t CircularBufferPushBack(CircularBufferContext *ctx, void *val) {
     size_t write_pos = (ctx->write_pos + 1) & ctx->max_size;
-
+    int retvar2;
     // Check if full
     if (write_pos == ctx->read_pos) {
 //    if (0 == CircularBufferSpace(&ctx)) {
-        return -1;
+        retvar2 = -1;
     }
-
+    else{
     memcpy(&ctx->buf[ctx->write_pos * ctx->element_size], val,
            ctx->element_size);
     ctx->write_pos = write_pos;
-
-    return 0;
+    
+    retvar2 = 0;
+    }
+    return retvar2;
 }
 
 int8_t CircularBufferPopFront(CircularBufferContext *ctx, void *val) {
     // Check if empty
+    int retvar3;
     if (ctx->read_pos == ctx->write_pos) {
-        return -1;
+        retvar3 = -1;
     }
-
-    memcpy(val, &ctx->buf[ctx->read_pos * ctx->element_size],
+    else{
+        memcpy(val, &ctx->buf[ctx->read_pos * ctx->element_size],
            ctx->element_size);
 
-    ctx->read_pos = (ctx->read_pos + 1) & ctx->max_size;
+        ctx->read_pos = (ctx->read_pos + 1) & ctx->max_size;
 
-    return 0;
+        retvar3 = 0;
+        }
+    return retvar3
 }
 
 int8_t CircularBufferPeek(const CircularBufferContext *ctx, size_t num,
                            void **elem) {
+    int retvar;
     size_t write_pos = ctx->write_pos;
     size_t read_pos = ctx->read_pos;
     size_t size = ((write_pos - read_pos) & ctx->max_size);
+    int sizeint2 = (int)size;
 
     // Check that the buffer isn't empty and
     // that num is less than number of added elements
-    if ((size == 0) || (size <= num)) {
-        goto fail;
+    if ((sizeint2 == 0) || (size <= num)) {
+        retvar = -1;
     }
 
-    size_t element_pos = ((read_pos + num) & ctx->max_size);
+    else{
+            size_t element_pos = ((read_pos + num) & ctx->max_size);
     *elem = &ctx->buf[element_pos * ctx->element_size];
 
-    return 0;
-
-fail:
-    return -1;
+    retvar = 0;
+}
+    return retvar
+   
+//fail:
+//    return -1;
 }
 
 size_t CircularBufferSize(const CircularBufferContext *ctx) {
