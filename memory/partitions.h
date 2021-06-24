32406922a96256af06bf067d94e8220c8a492c11
sramrajkar <55571093+sramrajkar@users.noreply.github.com>
2021-06-24 00:35:08 -0400

    Set up partition table

---
+++ memory/partitions.h
@@ -0,0 +1,20 @@
+#include "mcc.h"
+
+#define SIZE_OF_APP_PARTITION   131072
+
+typedef struct partition_t
+{
+    uint8_t name[32];
+    uint32_t startAddr;
+    uint32_t size;
+    uint16_t crc16;
+};
+
+partition_t partitions
+{
+    {"app_a", 0u, SIZE_OF_APP_PARTITION, 0u},
+    {"app_b", SIZE_OF_APP_PARTITION, SIZE_OF_APP_PARTITION, 0u},
+    {NULL, NULL, NULL, NULL}
+};
+
+uint8_t formatPartitions(void);
