27e6a1bae6d9e1f209ab0ba1826f7fdc6e4e8f5d
sramrajkar <55571093+sramrajkar@users.noreply.github.com>
2021-06-24 00:37:28 -0400

    redefining notion of partitions

--- memory/partitions.c
+++ memory/partitions.c
@@ -1,9 +1,7 @@
 #include "mcc.h"
 #include "partitions.h"
 
-uint8_t formatPartitions(void)
+uint8_t formatPartitions(uint8_t partitionid)
 {
-    /*Read each partition in the table form memory and see if CRC is ok.
-     If CRC on the partition fails then erase and format the partition.
-     CRC includes the 4kB header of the partition. Set CRC to 0 for calculations*/
+    /*This is simple erase for us*/
 }
